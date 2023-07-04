285fd53a8b495530c91efbfcd0bc976e282e1480
Hijikata1869 <liv.knk@gmail.com>
2023-07-04 18:05:16 +0900

    Auth.jsにニックネームのフォームを追加

--- components/Auth.js
+++ components/Auth.js
@@ -12,6 +12,25 @@ export default function Auth() {
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                ニックネーム
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="nickname"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
                 htmlFor="email"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
