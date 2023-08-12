aa4640a0555d9d3d0816c9d5b3cb4aaa34a54e1e
Hijikata1869 <liv.knk@gmail.com>
2023-08-12 21:34:13 +0900

    トレーニング記録を削除するときの確認用のモーダルを作成

---
+++ components/CheckDialog.js
@@ -0,0 +1,60 @@
+import { Dialog } from "@headlessui/react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+
+import { deleteTrainingLog } from "../lib/training-logs";
+
+export default function CheckDialog({
+  dialogOpen,
+  setIsOpen,
+  userId,
+  trainingLogId,
+}) {
+  const hundleClick = (e, userId, trainingLogId) => {
+    e.preventDefault();
+    try {
+      deleteTrainingLog(userId, trainingLogId).then((res) => {
+        if (res.status === 200) {
+          alert("削除成功！");
+        } else {
+          alert("削除失敗！");
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Dialog
+      open={dialogOpen}
+      onClose={() => setIsOpen(false)}
+      className="relative z-50"
+    >
+      <div className="fixed inset-0 bg-black/30" />
+      <div className="fixed inset-0 flex items-center justify-center p-4">
+        <Dialog.Panel className="flex flex-col items-center justify-center w-full max-w-xs rounded bg-white p-4 shadow-xl">
+          <div className="flex items-center justify-center mb-4">
+            <div className="mx-auto flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+              <ExclamationTriangleIcon className="h-4 w-4 text-red-600" />
+            </div>
+            <Dialog.Title className="ml-2 text-xl">削除しますか？</Dialog.Title>
+          </div>
+          <div className="flex">
+            <button
+              className="mt-3 mr-2 inline-flex w-auto justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              onClick={() => setIsOpen(false)}
+            >
+              キャンセル
+            </button>
+            <button
+              className="mt-3 ml-2 inline-flex w-auto rounded-md bg-red-600 text-sm font-semibold text-white px-3 py-2 shadow-sm hover:bg-red-500"
+              onClick={(e) => hundleClick(e, userId, trainingLogId)}
+            >
+              削除する
+            </button>
+          </div>
+        </Dialog.Panel>
+      </div>
+    </Dialog>
+  );
+}
--- pages/users/[id].js
+++ pages/users/[id].js
@@ -6,6 +6,7 @@ import { fetchCurrentUser, fetchUser, getAllUserIds } from "../../lib/users";
 import { fetchUserTrainingLogs } from "../../lib/training-logs";
 
 import Layout from "../../components/Layout";
+import CheckDialog from "../../components/CheckDialog";
 
 const trainingMenusArr = [
   "プッシュアップ",
@@ -52,6 +53,9 @@ export default function UserTrainingLogs({ userData, userTrainingLogsData }) {
   const [reps, setReps] = useState("１回");
   const [sets, setSets] = useState("１セット");
   const [memo, setMemo] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [userId, setUserId] = useState(0);
+  const [trainingLogId, setTrainingLogId] = useState(0);
   const stepsArr = makeSteps();
   const repsArr = makeReps();
   const setsArr = makeSets();
@@ -126,12 +130,7 @@ export default function UserTrainingLogs({ userData, userTrainingLogsData }) {
         }
       ).then((res) => {
         if (res.status === 200) {
-          // window.location.reload();
-          setTrainingMenu("プッシュアップ");
-          setStep("ステップ１");
-          setReps("１回");
-          setSets("１セット");
-          setMemo("");
+          window.location.reload();
         }
       });
     } catch (err) {
@@ -139,154 +138,201 @@ export default function UserTrainingLogs({ userData, userTrainingLogsData }) {
     }
   };
 
+  const hundleClick = (userId, trainingLogId) => {
+    setIsOpen(true);
+    setUserId(userId);
+    setTrainingLogId(trainingLogId);
+  };
+
+  // const deleteTrainingLogAction = async () => {
+  //   try {
+  //     await fetch(
+  //       `${process.env.NEXT_PUBLIC_RAILSAPI_URL}/users/${user.id}/training_logs/${id}`,
+  //       {
+  //         method: "DELETE",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         credentials: "include",
+  //       }
+  //     ).then((res) => {
+  //       if (res.status === 200) {
+  //         alert("削除完了！");
+  //       }
+  //     });
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
   return (
-    <Layout title={`${user.name}さんのトレーニング記録`}>
-      <div className="lg:w-96">
-        {user.id === currentUser?.id ? (
-          <>
-            <h1 className="mb-6 font-bold text-2xl">トレーニングを記録する</h1>
-            <div className="pb-20">
-              <label
-                htmlFor="trainingMenu"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                トレーニングメニュー
-              </label>
-              <select
-                id="trainingMenu"
-                className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                value={trainingMenu}
-                onChange={hundleValueChange}
-              >
-                {trainingMenusArr.map((trainingMenu, index) => {
-                  return (
-                    <option
-                      key={index}
-                      value={trainingMenu}
-                    >{`${trainingMenu}`}</option>
-                  );
-                })}
-              </select>
-              <label
-                htmlFor="step"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                ステップ
-              </label>
-              <select
-                id="step"
-                className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                value={step}
-                onChange={hundleValueChange}
-              >
-                {stepsArr.map((step, index) => {
-                  return (
-                    <option key={index} value={`${step}`}>{`${step}`}</option>
-                  );
-                })}
-              </select>
-              <label
-                htmlFor="reps"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                回数
-              </label>
-              <select
-                id="reps"
-                className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                value={reps}
-                onChange={hundleValueChange}
-              >
-                {repsArr.map((rep, index) => {
-                  return <option key={index} value={rep}>{`${rep}`}</option>;
-                })}
-              </select>
-              <label
-                htmlFor="sets"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                セット数
-              </label>
-              <select
-                id="sets"
-                className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                value={sets}
-                onChange={hundleValueChange}
-              >
-                {setsArr.map((set, index) => {
-                  return (
-                    <option key={index} value={`${set}`}>{`${set}`}</option>
-                  );
-                })}
-              </select>
-              <label
-                htmlFor="memo"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                一言メモ
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="memo"
-                  rows="3"
-                  className="bg-gray-50 block w-full rounded-lg border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  value={memo}
+    <>
+      <CheckDialog
+        dialogOpen={isOpen}
+        setIsOpen={setIsOpen}
+        userId={userId}
+        trainingLogId={trainingLogId}
+      />
+      <Layout title={`${user.name}さんのトレーニング記録`}>
+        <div className="lg:w-96">
+          {user.id === currentUser?.id ? (
+            <>
+              <h1 className="mb-6 font-bold text-2xl">
+                トレーニングを記録する
+              </h1>
+              <div className="pb-20">
+                <label
+                  htmlFor="trainingMenu"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  トレーニングメニュー
+                </label>
+                <select
+                  id="trainingMenu"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={trainingMenu}
                   onChange={hundleValueChange}
-                ></textarea>
-              </div>
-              <button
-                className="bg-indigo-500  px-2 py-2 mt-10 w-full text-white rounded-md"
-                onClick={(e) => postTrainingLogAction(e)}
-              >
-                送信
-              </button>
-            </div>
-          </>
-        ) : null}
-        <div>
-          <h1 className="font-bold text-2xl">{`${user.name}さんのトレーニング記録`}</h1>
-          {userTrainingLogs?.length ? (
-            userTrainingLogs.map((trainingLog, index) => {
-              return (
-                <div key={index} className="flex flex-col p-4 w-80">
-                  <div className="flex justify-center my-4 border-t border-gray-400 pt-2">
-                    <p className="px-1 text-lg">{`${trainingLog.training_menu}`}</p>
-                  </div>
-                  <div className="flex flex-row justify-between">
-                    <div className="mb-2">
-                      <p className="text-xs text-gray-500">ステップ</p>
-                      <p className="">{`${trainingLog.step}`}</p>
-                    </div>
-                    <div className="mb-2 mr-5">
-                      <p className="text-xs text-gray-500">回数</p>
-                      <p>{`${trainingLog.repetition}`}</p>
-                    </div>
-                    <div className="mb-2">
-                      <p className="text-xs text-gray-500">セット数</p>
-                      <p>{`${trainingLog.set}`}</p>
-                    </div>
-                  </div>
-                  <div>
-                    <p className="text-xs text-gray-500">メモ</p>
-                    <p>{`${trainingLog.memo}`}</p>
-                  </div>
+                >
+                  {trainingMenusArr.map((trainingMenu, index) => {
+                    return (
+                      <option
+                        key={index}
+                        value={trainingMenu}
+                      >{`${trainingMenu}`}</option>
+                    );
+                  })}
+                </select>
+                <label
+                  htmlFor="step"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  ステップ
+                </label>
+                <select
+                  id="step"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={step}
+                  onChange={hundleValueChange}
+                >
+                  {stepsArr.map((step, index) => {
+                    return (
+                      <option key={index} value={`${step}`}>{`${step}`}</option>
+                    );
+                  })}
+                </select>
+                <label
+                  htmlFor="reps"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  回数
+                </label>
+                <select
+                  id="reps"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={reps}
+                  onChange={hundleValueChange}
+                >
+                  {repsArr.map((rep, index) => {
+                    return <option key={index} value={rep}>{`${rep}`}</option>;
+                  })}
+                </select>
+                <label
+                  htmlFor="sets"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  セット数
+                </label>
+                <select
+                  id="sets"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={sets}
+                  onChange={hundleValueChange}
+                >
+                  {setsArr.map((set, index) => {
+                    return (
+                      <option key={index} value={`${set}`}>{`${set}`}</option>
+                    );
+                  })}
+                </select>
+                <label
+                  htmlFor="memo"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  一言メモ
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="memo"
+                    rows="3"
+                    className="bg-gray-50 block w-full rounded-lg border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={memo}
+                    onChange={hundleValueChange}
+                  ></textarea>
                 </div>
-              );
-            })
-          ) : (
-            <p className="pt-4">トレーニング記録はありません</p>
-          )}
+                <button
+                  className="bg-indigo-500  px-2 py-2 mt-10 w-full text-white rounded-md"
+                  onClick={(e) => postTrainingLogAction(e)}
+                >
+                  送信
+                </button>
+              </div>
+            </>
+          ) : null}
+          <div>
+            <h1 className="font-bold text-2xl">{`${user.name}さんのトレーニング記録`}</h1>
+            {userTrainingLogs?.length ? (
+              userTrainingLogs.map((trainingLog, index) => {
+                return (
+                  <div key={index} className="flex flex-col p-4 w-80">
+                    <div className="flex justify-center my-4 border-t border-gray-400 pt-2">
+                      <p className="px-1 text-lg">{`${trainingLog.training_menu}`}</p>
+                    </div>
+                    <div className="flex flex-row justify-between">
+                      <div className="mb-2">
+                        <p className="text-xs text-gray-500">ステップ</p>
+                        <p className="">{`${trainingLog.step}`}</p>
+                      </div>
+                      <div className="mb-2 mr-5">
+                        <p className="text-xs text-gray-500">回数</p>
+                        <p>{`${trainingLog.repetition}`}</p>
+                      </div>
+                      <div className="mb-2">
+                        <p className="text-xs text-gray-500">セット数</p>
+                        <p>{`${trainingLog.set}`}</p>
+                      </div>
+                    </div>
+                    <div className="mb-2">
+                      <p className="text-xs text-gray-500">メモ</p>
+                      <p>{`${trainingLog.memo}`}</p>
+                    </div>
+                    <div className="flex justify-center">
+                      <button
+                        className="mt-2 inline-flex w-auto rounded-md bg-red-500 text-sm text-white px-3 py-2 shadow-sm hover:bg-red-400"
+                        onClick={() =>
+                          hundleClick(trainingLog.user_id, trainingLog.id)
+                        }
+                      >
+                        削除する
+                      </button>
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+              <p className="pt-4">トレーニング記録はありません</p>
+            )}
+          </div>
+          {/* <div>
+            <button
+              className="bg-gray-500 p-10"
+              onClick={() => console.log(userTrainingLogs)}
+            >
+              PROPS
+            </button>
+          </div> */}
         </div>
-        <div>
-          <button
-            className="bg-gray-500 p-10"
-            onClick={() => console.log(userTrainingLogs)}
-          >
-            PROPS
-          </button>
-        </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 }
 
